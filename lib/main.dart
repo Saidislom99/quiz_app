6b5a4ff2506421a46ee37caf4911f10b680a7279
Saidislom99 <saidislomnajimov@gmail.com>
2022-10-06 22:09:41 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_quiz_app/screens/my_quiz_screen.dart';
+import 'package:my_quiz_app/screens/select_quiz_screen.dart';
+import 'package:my_quiz_app/screens/splash_screen.dart';
+
+void main() async {
+  runApp(MyQuizApp());
+}
+
+class MyQuizApp extends StatelessWidget {
+  const MyQuizApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 812),
+      splitScreenMode: true,
+      minTextAdapt: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: child,
+        );
+      },
+      child: SplashScreen(),
+    );
+  }
+}
